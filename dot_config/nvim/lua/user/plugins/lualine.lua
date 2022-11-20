054a8baaba5b2a0e66ac1e6fd9ad708160fff53c
Joe George <joegeorge01@gmail.com>
2022-11-20 09:47:55 -0500

    Update

--- dot_config/nvim/lua/user/plugins/lualine.lua
+++ dot_config/nvim/lua/user/plugins/lualine.lua
@@ -8,55 +8,68 @@ M.config = function()
   theme.hi('StatusLine', { bg = '' })
   theme.hi('StatusLineNC', { bg = '' })
 
+  local refresh_time = 500
+
   local config = {
     options = {
       theme = 'wezterm',
       section_separators = { left = ' ', right = ' ' },
       component_separators = { left = '│', right = '│' },
       globalstatus = true,
+      refresh = {
+        -- match refresh time of block containing lsp_progress block to the
+        -- lsp_progress timer
+        statusline = refresh_time,
+      },
     },
     sections = {
-      lualine_a = { 'mode' },
       lualine_b = {
+        -- { 'branch', icon = '', padding = { left = 0, right = 1 } },
         {
-          'filetype',
-          separator = '',
+          'diagnostics',
+          sources = { 'nvim_diagnostic' },
           padding = { left = 0, right = 1 },
-          icon_only = true,
         },
+        { 'diff', padding = { left = 0, right = 1 } },
       },
       lualine_c = {
-        { 'filename', path = 1, padding = { left = 0, right = 1 } },
-      },
-      lualine_x = {
-        { 'diagnostics', sources = { 'nvim_diagnostic' } },
-        {
-          'language_servers',
-          separator = '',
-          padding = { right = 0, left = 1 },
-        },
         {
           'lsp_progress',
           display_components = { 'spinner' },
-          spinner_symbols = { '⠖', '⠲', '⠴', '⠦' },
-          timer = { spinner = 250 },
-          padding = { left = 1, right = 0 },
+          spinner_symbols = { '⠦', '⠖', '⠲', '⠴' },
+          -- spinner_symbols = { '⠐', '⠠', '⠄', '⠂' },
+          timer = { spinner = refresh_time },
+          padding = { left = 0, right = 0 },
         },
       },
-      lualine_z = { 'location' },
+      lualine_x = {},
+      lualine_y = {
+        { 'progress', padding = { left = 1, right = 0 } },
+      },
     },
-    inactive_sections = {
-      lualine_c = { 'filename' },
-      lualine_x = { 'location' },
+    winbar = {
+      lualine_c = {
+        { 'filetype', icon_only = 1 },
+        { 'filename', path = 1, padding = { left = 1, right = 1 } },
+      },
+      lualine_x = {
+        {
+          'language_servers',
+          separator = '',
+          padding = { left = 1, right = 1 },
+        },
+      },
     },
-    extensions = { 'nvim-tree', 'quickfix', 'symbols_outline' },
+    extensions = { 'nvim-tree', 'quickfix' },
   }
+
+  config.inactive_winbar = config.winbar
 
   local navic = req('nvim-navic')
   if navic then
     table.insert(
       config.sections.lualine_c,
-      { navic.get_location, cond = navic.is_available }
+      { navic.get_location, cond = navic.is_available, padding = { left = 0 } }
     )
   end
 
